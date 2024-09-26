3b6238a5f757df61c11b95fa42af71feabc6b63c
Sakar Aryal <techiesakar@gmail.com>
2024-09-26 20:59:23 +0545

    mapping array no reuse

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,69 @@
 import { Button } from "./components/button"
 
 function App() {
+  const users = [{
+    name: "chirinjivi",
+    email: "chirinjivi@gmail.com"
+  },
+  {
+    name: "sakar",
+    email: "sakar@gmail.com"
+  }
+  ]
   return (
     <div>
       <form>
-        <input  type="text" placeholder="First Name" /> <br />
+        <input type="text" placeholder="First Name" /> <br />
         <input type="text" placeholder="Email" /> <br />
-        <Button label="Submit" className="text-orange-500"/>
-        <Button label="Click Here" className="text-red-500 bg-purple-700"/>
-        <Button label="Read More"/>
+        <Button label="Submit" className="text-orange-500" />
+        <Button label="Click Here" className="text-red-500 bg-purple-700" />
+        <Button label="Read More" />
+
+        {/* card  zero*/}
+        <div className="shadow-md rounded-lg w-80 border bg-slate-50 p-8 text-center">
+          <h1 className="text-2xl font-bold text-slate-800">
+            {users[0].name}
+          </h1>
+          <p className="text-lg text-slate-600">
+            {users[0].email}
+          </p>
+        </div>
+
+        {/* card zero ends */}
+
+
+        {/* card  one*/}
+        <div className="shadow-md rounded-lg w-80 border bg-slate-50 p-8 text-center">
+          <h1 className="text-2xl font-bold text-slate-800">
+            {users[1].name}
+          </h1>
+          <p className="text-lg text-slate-600">
+            {users[1].email}
+          </p>
+        </div>
+
+        {/* card one ends */}
+
+
+        {/* let's map the array */}
+
+        <p className="my-20">Array is mapping</p>
+
+
+        {users.map((item, index) => {
+          return (
+            <div key={index} className="shadow-md rounded-lg w-80 border bg-slate-50 p-8 text-center">
+              <h1 className="text-2xl font-bold text-slate-800">
+                {item.name}
+              </h1>
+              <p className="text-lg text-slate-600">
+                {item.email}
+              </p>
+            </div>
+          )
+        })}
+
+
       </form>
     </div>
   )
